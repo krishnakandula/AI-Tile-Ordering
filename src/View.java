ff8819a2a6babddc639b41943c3d3e90c3ecfb99
krishnakandula <krishna.c.kandula@gmail.com>
2017-02-07 09:15:08 -0600

    Added command line args support

--- src/View.java
+++ src/View.java
@@ -6,6 +6,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 /**
  * Created by Krishna Chaitanya Kandula on 2/3/17.
@@ -15,17 +16,41 @@ public class View {
     private static final String INPUT_FILE_PATH = new File("")
             .getAbsolutePath()
             .concat("/input.txt");
-
+    
     public static void main(String... args){
         //TODO: Get commandline arguments
-        String input = getInput();
-        StateController controller = new UCSController();
-        Driver driver = new Driver(controller, false);
+        System.out.println(Arrays.toString(args));
+
+        if(args.length < 3 || args.length > 4) {
+            System.out.println("Invalid arguments: " + Arrays.toString(args));
+            System.exit(0);
+        }
+
+        if(!args[0].equals("search")) {
+            System.out.println(args[0] + " command not found");
+            System.exit(0);
+        }
+
+        boolean costFlag = false;
+        String inputFileName = "input.txt";
+        StateController controller = new BFSController();
+        if(args[1].equals("-cost")){
+            costFlag = true;
+            controller = ControllerFactory.getController(args[2]);
+            inputFileName = args[3];
+        } else {
+            controller = ControllerFactory.getController(args[1]);
+            inputFileName = args[2];
+        }
+
+        INPUT_FILE_PATH.concat(inputFileName);
+        String input = getTileInput();
+        Driver driver = new Driver(controller, costFlag);
         State finalState = driver.runAlgorithm(input);
         System.out.println(driver.getFinalPath(finalState));
     }
 
-    private static String getInput(){
+    private static String getTileInput(){
         //Initialize readers
         String initialState = null;
         FileReader fileReader = null;
---
+++ src/controllers/ControllerFactory.java
@@ -0,0 +1,22 @@
+package controllers;
+
+/**
+ * Created by Krishna Chaitanya Kandula on 2/7/2017.
+ */
+public class ControllerFactory {
+    public static StateController getController(String controllerType){
+        controllerType = controllerType.toLowerCase();
+        switch (controllerType){
+            case "DFS":
+                return new DFSController();
+            case "UCS":
+                return new UCSController();
+            case "GS":
+                return new GSController();
+            case "A-star":
+                return new AStarController();
+            default:
+                return new BFSController();
+        }
+    }
+}
